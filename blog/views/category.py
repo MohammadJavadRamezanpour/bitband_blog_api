8710083b84684fcebefb20f53224577dbb094649
Mohammad Javad <ramezanpourmohammadjavad@gmail.com>
2023-05-26 13:05:19 +0430

    made progress

--- blog/views/category.py
+++ blog/views/category.py
@@ -1,13 +1,7 @@
-from django.db.models import Q
-
 from rest_framework import viewsets
-from rest_framework.decorators import action
-from rest_framework.permissions import IsAdminUser
-from rest_framework.response import Response
 
 from blog.serializers import CategorySerializer
 from blog.models import Category
-from user.permissions import IsUserManager
 
 
 class CategoryViewset(viewsets.ModelViewSet):
